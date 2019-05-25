4fbf415c0428d73555ad962dbb0519c0d8a05abc
vladkyr <48084668+vladkyr@users.noreply.github.com>
2019-05-25 13:43:52 +0200

    Update battleships.ino

--- battleships.ino
+++ battleships.ino
@@ -116,36 +116,34 @@ void printBigXblink(){
 
 void drawCircle(int radius) {
   if (radius == 0) {
-    printPoint(3,3);
-    printPoint(3,4);
-    printPoint(4,4);
-    printPoint(4,3);
+     printPoint(3,3);
+     printPoint(3,4);
+     printPoint(4,4);
+     printPoint(4,3);
   }
-  if (radius==1){
-      drawCircle(0);
-      delay(300);
-      printPoint(3,2);
-      printPoint(4,2);
-      printPoint(2,3);
-      printPoint(2,4);
-      printPoint(3,6);
-      printPoint(4,6);
-      printPoint(5,3);
-      printPoint(5,4);
+  if (radius==1) {
+     printPoint(3,2);
+     printPoint(4,2);
+     printPoint(2,3);
+     printPoint(2,4);
+     printPoint(3,6);
+     printPoint(4,6);
+     printPoint(5,3);
+     printPoint(5,4);
   }
-  if (radius==2){
-      printPoint(3,1);
-      printPoint(4,1);
-      printPoint(2,2);
-      printPoint(1,3);
-      printPoint(1,4);
-      printPoint(2,5);
-      printPoint(3,6);
-      printPoint(4,6);
-      printPoint(5,5);
-      printPoint(6,3);
-      printPoint(6,4);
-      printPoint(5,2);
+  if (radius==2) {
+     printPoint(3,1);
+     printPoint(4,1);
+     printPoint(2,2);
+     printPoint(1,3);
+     printPoint(1,4);
+     printPoint(2,5);
+     printPoint(3,6);
+     printPoint(4,6);
+     printPoint(5,5);
+     printPoint(6,3);
+     printPoint(6,4);
+     printPoint(5,2);
   }
   if (radius==3) {
     printPoint(3,0);
@@ -182,5 +180,42 @@ void drawCircle(int radius) {
     printPoint(7,4);
     printPoint(6,2);
     printPoint(5,1);
+  }
+  if (radius==5) {
+    printPoint(2,0);
+    printPoint(1,1);
+    printPoint(0,2);
+    printPoint(0,5);
+    printPoint(1,6);
+    printPoint(2,7);
+    printPoint(5,7);
+    printPoint(6,6);
+    printPoint(7,5);
+    printPoint(7,2);
+    printPoint(6,1);
+    printPoint(5,0);
+  }
+  if (radius==6) {
+    printPoint(1,0);
+    printPoint(0,1);
+    printPoint(0,6);
+    printPoint(1,7);
+    printPoint(6,7);
+    printPoint(7,6);
+    printPoint(7,1);
+    printPoint(6,0);
+  }
+  if (radius==7) {
+    printPoint(0,0);
+    printPoint(0,7);
+    printPoint(7,7);
+    printPoint(7,0);
+  }
+}
+
+void drawBoom(){
+  for (int i=0;i<8;++i) {
+    drawCircle(i);
+    delay(300); // Maybe must put bigger delay
   }
 }
