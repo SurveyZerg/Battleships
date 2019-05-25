2b5a327f4403f6fb4882d0ae188fadfc13a42145
vladkyr <48084668+vladkyr@users.noreply.github.com>
2019-05-25 13:46:53 +0200

    Update drawBattleships.h

--- drawBattleships.h
+++ drawBattleships.h
@@ -1,2 +1,151 @@
 // Library for Battleships for drawing different things
 
+
+void printPoint(int x, int y) {
+   mx.setPoint(x,y, true);
+}
+
+void clearPoint(int x, int y) {
+  mx.setPoint(x,y, false);
+}
+
+void blinkingPoint(int x,int y) {
+  mx.setPoint(x,y, true);
+  delay(100);
+  mx.setPoint(x,y, false);
+  delay(100);
+}
+
+void printRow(int x ) {
+  for(int i=0; i<8;++i) 
+      printPoint(x,i);
+}
+
+void printColumn(int y) {
+  for(int i=0; i<8;++i) 
+      printPoint(i,y);
+}
+
+void printBigX() {
+  for(int i=0; i<8;++i) 
+      printPoint(i,i);
+  for(int i=0; i<8;++i) 
+      printPoint(i,7-i);
+}
+
+void printBigXblink() {
+  for(int i=0; i<8;++i) {
+      printPoint(i,i);
+      delay(random(100));
+  }
+  for(int i=0; i<8;++i) {
+      printPoint(i,7-i);
+      delay(random(100));
+  }
+}
+
+void drawCircle(int radius) {
+  if (radius == 0) {
+     printPoint(3,3);
+     printPoint(3,4);
+     printPoint(4,4);
+     printPoint(4,3);
+  }
+  if (radius==1) {
+     printPoint(3,2);
+     printPoint(4,2);
+     printPoint(2,3);
+     printPoint(2,4);
+     printPoint(3,6);
+     printPoint(4,6);
+     printPoint(5,3);
+     printPoint(5,4);
+  }
+  if (radius==2) {
+     printPoint(3,1);
+     printPoint(4,1);
+     printPoint(2,2);
+     printPoint(1,3);
+     printPoint(1,4);
+     printPoint(2,5);
+     printPoint(3,6);
+     printPoint(4,6);
+     printPoint(5,5);
+     printPoint(6,3);
+     printPoint(6,4);
+     printPoint(5,2);
+  }
+  if (radius==3) {
+    printPoint(3,0);
+    printPoint(4,0);
+    printPoint(2,1);
+    printPoint(1,2);
+    printPoint(0,3);
+    printPoint(0,4);
+    printPoint(1,5);
+    printPoint(2,6);
+    printPoint(3,7);
+    printPoint(4,7);
+    printPoint(5,6);
+    printPoint(6,5);
+    printPoint(7,3);
+    printPoint(7,4);
+    printPoint(6,2);
+    printPoint(5,1);
+  }
+  if (radius==4) {
+    printPoint(3,0);
+    printPoint(4,0);
+    printPoint(2,1);
+    printPoint(1,2);
+    printPoint(0,3);
+    printPoint(0,4);
+    printPoint(1,5);
+    printPoint(2,6);
+    printPoint(3,7);
+    printPoint(4,7);
+    printPoint(5,6);
+    printPoint(6,5);
+    printPoint(7,3);
+    printPoint(7,4);
+    printPoint(6,2);
+    printPoint(5,1);
+  }
+  if (radius==5) {
+    printPoint(2,0);
+    printPoint(1,1);
+    printPoint(0,2);
+    printPoint(0,5);
+    printPoint(1,6);
+    printPoint(2,7);
+    printPoint(5,7);
+    printPoint(6,6);
+    printPoint(7,5);
+    printPoint(7,2);
+    printPoint(6,1);
+    printPoint(5,0);
+  }
+  if (radius==6) {
+    printPoint(1,0);
+    printPoint(0,1);
+    printPoint(0,6);
+    printPoint(1,7);
+    printPoint(6,7);
+    printPoint(7,6);
+    printPoint(7,1);
+    printPoint(6,0);
+  }
+  if (radius==7) {
+    printPoint(0,0);
+    printPoint(0,7);
+    printPoint(7,7);
+    printPoint(7,0);
+  }
+}
+
+void drawBoom(){
+  for (int i=0;i<8;++i) {
+    drawCircle(i);
+    delay(300); // Maybe must put bigger delay
+  }
+}
